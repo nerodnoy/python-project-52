205fd4f0196f57d24e6f2bfe0f38992cbe354488
nerodnoy <thatsforsure@mail.ru>
2024-07-20 14:27:52 +0300

    added paths and views

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import *
+from task_manager import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', views.index, name='index'),
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Welcome to Task Manager")
+    return render(request, 'index.html')
