4d2f456b4094cea3d6f317bd9fa9cea398cd994e
nerodnoy <thatsforsure@mail.ru>
2024-07-30 14:03:00 +0300

    moved flash-messages to separated template

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
@@ -8,11 +10,18 @@ class IndexView(TemplateView):
     template_name = 'index.html'
 
 
-class UserLoginView(LoginView):
+class UserLoginView(SuccessMessageMixin, LoginView):
     template_name = 'login.html'
     form_class = AuthenticationForm
     next_page = reverse_lazy('home')
 
+    success_message = 'You are logged in'
+
 
 class UserLogoutView(LogoutView):
     next_page = reverse_lazy('home')
+
+    # Logout flash-message
+    def dispatch(self, request, *args, **kwargs):
+        messages.info(request, 'You are logged out')
+        return super().dispatch(request, *args, **kwargs)
