c298c101bc1518481c4e9e6bec3b8af69e7a4a2e
nerodnoy <thatsforsure@mail.ru>
2024-08-08 23:14:32 +0300

    translations fixes

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -3,13 +3,15 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.db.models import ProtectedError
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
+from django.utils.translation import gettext as _
 
 
 class AuthRequiredMixin(LoginRequiredMixin):
 
+    authorization_message = _('You are not authorized! Please, log in')
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            messages.error(request, 'You are not authorized! Please, log in')
+            messages.error(request, self.authorization_message)
             return redirect(reverse_lazy('login'))
 
         return super().dispatch(request, *args, **kwargs)
